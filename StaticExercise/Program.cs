d6e198d506eb5bbfe246d070ea1117f45372ba91
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-25 12:06:58 -0400

    Exorcise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please input temperature in fahrenheit to convert to celsius.");
+
+            double farTempInput;
+            while (!double.TryParse(Console.ReadLine(), out farTempInput))
+            {
+                Console.WriteLine("Please input a valid value.");
+            }
+
+            double celsius = TempConverter.FahrenheitToCelsius(farTempInput);
+
+            Console.WriteLine("Please input temperature in celsius to convert to fahrenheit.");
+
+            double celTempInput;
+            while (!double.TryParse(Console.ReadLine(), out celTempInput))
+            {
+                Console.WriteLine("Please input a valid value.");
+            }
+
+            double fahrenheit = TempConverter.CelsiusToFahrenheit(celTempInput);
+
+            Console.WriteLine(
+                $"{farTempInput} degrees fahrenheit converts to {celsius.ToString("F2")} degrees celsius.\n"
+                + $"{celTempInput} degrees celsius converts to {fahrenheit.ToString("F2")} degrees fahrenheit.\n");
+
+            Console.WriteLine("Press enter to quit.\n");
+            Console.ReadLine();
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static public class TempConverter
+    {
+        static public double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) / 1.8;
+        }
+
+        static public double CelsiusToFahrenheit(double c)
+        {
+            return (c * 1.8) + 32;
+        }
+    }
+}
